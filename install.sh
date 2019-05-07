06e5a368badea1a0fc46ed91ec7ff4ae841641b9
Dennis Wulfert <dennis.wulfert@perfact.de>
2019-05-07 14:08:22 +0200

    hot fix for installer, installer runs into errors

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt-get install python3-dev -y
 sudo apt-get install python3.7-dev -y
 sudo apt-get install python3.7 -y
 sudo apt-get install python3.7-venv -y
+sudo rm -rf Zope/ pip/
 cd ..
 python3.7 -m venv zope;
 cd zope
