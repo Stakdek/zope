1100cdf78a6f56407399c6b9327b35dc5d1d4f4e
Lars Bergmann <lars.bergmann@perfact.de>
2019-05-14 22:47:09 +0200

    Enable zodbsync, add example config and usage guide.

---
+++ zodbsync_config.py
@@ -0,0 +1,28 @@
+# Path of the Zope instance configuration to use to
+# instantiate Zope2.app()
+#conf_path = '/var/lib/zope2.13/instance/ema/etc/zope.conf'
+wsgi_conf_path = '/path/to/this/repo/app/instance/etc/wsgi.conf'
+
+# Path to Data.fs which is needed for lookup of object IDs from transaction IDs
+# with perfact-zoperecord --watch
+datafs_path = '/path/to/this/repo/app/instance/var/Data.fs'
+
+# user that is used to create commits and as default owner of objects
+manager_user = 'admin'
+
+# create the manager user on empty databases
+create_manager_user = False
+
+# sets the default owner for objects that have no owner in the file system representation
+default_owner = 'admin'
+
+# Base directory of the repository
+base_dir = '/place/to/keep/such/repos'
+
+# default settings for git repos
+commit_name = "Zope Developer"
+commit_email = "zope-devel@example.de"
+commit_message = "Generic commit message."
+
+# email address to send commit summaries of default commits to
+#codechange_mail = "zope-devel@example.de"
