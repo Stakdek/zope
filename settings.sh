d19a93e38215b9e13427717ff6e0274b486d9229
Dennis Wulfert <dennis.wulfert@perfact.de>
2020-02-04 12:52:01 +0100

    raise patch version of zope

--- settings.sh
+++ settings.sh
@@ -5,7 +5,7 @@
 BASE_PYTHON=/usr/bin/python3.7
 
 # zope release to be installed.
-ZOPE_RELEASE="4.1.1"
+ZOPE_RELEASE="4.1.3"
 
 # external app repository cloned into app folder after installation. optional.
 EXTERNAL_ZOPE_DATA=
